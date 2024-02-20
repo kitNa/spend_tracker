45987cac1c7c7d4f8be439dfceae077c4b729998
Kateryna Nahorna <kitnahorna@gmail.com>
2024-02-20 12:34:01 +0200

    account information entry screen has been added

--- lib/pages/accounts/account_page.dart
+++ lib/pages/accounts/account_page.dart
@@ -1,16 +1,79 @@
 import 'package:flutter/material.dart';
 
-class AccountPage extends StatelessWidget {
+class AccountPage extends StatefulWidget {
   const AccountPage({super.key});
 
+  @override
+  State<AccountPage> createState() => _AccountPageState();
+}
+
+class _AccountPageState extends State<AccountPage> {
+  Map<String, dynamic> _data = Map<String, dynamic>();
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.orangeAccent,
         title: const Text('Account'),
+        actions: <Widget>[
+          IconButton(
+              onPressed: (){
+                if(!_formKey.currentState!.validate())return;
+                _formKey.currentState!.save();
+                Navigator.of(context).pop();
+              },
+              icon: const Icon(Icons.save))
+        ],
       ),
-      body: const Center(
-        child: Text('Account'),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: <Widget> [
+              Container(
+                height: 100,
+                width: 100,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width:2,
+                    color: Colors.black87,
+                  ),
+                ),
+              ),
+              //Существует два виджета, которые позволяют захватывать текстовый
+              // ввод: Text Field и TextFormField. Оба наследуются от
+              // StatefulWidget, но TextFormField является производным
+              // непосредственно от класса FormField. Класс FormField добавляет
+              // дополнительные вспомогательные функции, которые при использовании
+              // с виджетом Form упрощают проверку и сохранение данных из
+              // нескольких полей одновременно.
+              TextFormField(
+                decoration: const InputDecoration (
+                  labelText: 'Name',
+                ),
+                validator: (var value){
+                  if(value == null || value.isEmpty) return 'Required';
+                },
+                onSaved: (value) => _data['name'] = value,
+              ),
+              TextFormField(
+                keyboardType:TextInputType.numberWithOptions(
+                    decimal: true
+                ) ,
+                decoration: const InputDecoration(
+                  labelText: 'Balance',
+                ),
+                validator: (var value) {
+                  if(value == null || value.isEmpty) return 'Required';
+                  if(double.tryParse(value) == null) return 'Invalid number';
+                },
+                onSaved: (value) => _data['balance'] = double.parse(value!),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/pages/accounts/accounts_page.dart
+++ lib/pages/accounts/accounts_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'account_page.dart';
+
 class AccountsPage extends StatelessWidget {
   const AccountsPage({super.key});
 
@@ -7,9 +9,20 @@ class AccountsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.orangeAccent,
-        title: const Text('Accounts'),
-      ),
+          backgroundColor: Colors.orangeAccent,
+          title: const Text('Accounts'),
+          actions: <Widget>[
+            IconButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const AccountPage(),
+                      )
+                  );
+                },
+                icon: const Icon(Icons.add)),
+          ]),
       body: const Center(
         child: Text('Accounts'),
       ),
