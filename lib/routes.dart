1a23090e6f36df170cd9cf6078bcf0274b66922e
Kateryna Nahorna <kitnahorna@gmail.com>
2024-03-29 11:03:36 +0200

    Listen for route changes home_page and item_page was implemented

--- lib/routes.dart
+++ lib/routes.dart
@@ -2,9 +2,17 @@ import 'package:flutter/cupertino.dart';
 import 'package:spend_tracker/pages/index.dart';
 
 final routes = {
-  '/': (BuildContext context) => HomePage(),
+  '/': (BuildContext context) => const HomePage(),
   '/accounts': (BuildContext context) => const AccountsPage(),
   '/items': (BuildContext context) => const ItemsPage(),
   '/types': (BuildContext context) => const TypesPage(),
   '/bar_chart': (BuildContext context) => const BarChartPage(),
 };
+
+// Flutter дозволяє нам підключитися до системи маршрутизації за допомогою
+// кількох класів: RouteObserver і RouteAware. RouteAware — це міксин, який ми
+// додаємо до віджета, який хоче підписатися на події маршруту. RouteObserver —
+// це клас, на який підписуються віджети RouteAware. RouteObserver сповіщає
+// віджети RouteAware про зміни маршруту.
+
+final RouteObserver<PageRoute> routeObserver = RouteObserver<PageRoute>();
